68e1df79c3b0f877dc88e218ed5df76fe7ab5578
Sergio Pessegueiro <iFrancisco.sp@icloud.com>
2021-08-04 16:28:34 +0100

    Add More tests

--- AdvancedNUnit/AdvancedNUnit/CalculatorTests.cs
+++ AdvancedNUnit/AdvancedNUnit/CalculatorTests.cs
@@ -41,6 +41,38 @@ namespace AdvancedNUnit
             Assert.That(strResult,Is.EqualTo("advancedNUnit.calculator").IgnoreCase);
             Assert.That(strResult, Is.Not.Empty);
         }
+
+        [Test]
+        public void TestArrayOfStrings()
+        {
+            var fruit = new List<string>()
+            {
+                "apple",
+                "pear",
+                "banana",
+                "peach"
+            };
+            
+            Assert.That(fruit, Does.Contain("apple"));
+            Assert.That(fruit, Has.Count.EqualTo(4));
+            Assert.That(fruit, Has.Exactly(2).StartsWith("pea"));
+        }
+
+        [Test]
+        public void TestRange()
+        {
+            Assert.That(8, Is.InRange(1,10));
+            var myList = new List<int>{4,2,7,5,9};
+            Assert.That(myList,Is.All.InRange(1,10));
+            Assert.That(myList, Has.Exactly(3).InRange(1,5));
+        }
+
+        [Test]
+        public void StringTest()
+        {
+            const string name = "Sergio";
+            Assert.That(name, Does.Contain("io"));
+        }
     }
     
 }
