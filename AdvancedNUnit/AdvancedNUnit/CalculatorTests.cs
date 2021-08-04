0a1b2f3c964e6b426e75b0e33c7f64a73fa25585
Sergio Pessegueiro <iFrancisco.sp@icloud.com>
2021-08-04 15:59:04 +0100

    Started using Rider.

--- AdvancedNUnit/AdvancedNUnit/CalculatorTests.cs
+++ AdvancedNUnit/AdvancedNUnit/CalculatorTests.cs
@@ -19,6 +19,28 @@ namespace AdvancedNUnit
             // Assert
             Assert.That(result, Is.EqualTo(expectedResult), "optional failure message");
         }
+
+        [Test]
+        public void SomeConstraints()
+        {
+            var subject = new Calculator { Num1 = 6 };
+            Assert.That(subject.DivisibleBy3());
+            subject.Num1 = 7;
+            Assert.That(subject.DivisibleBy3(), Is.False);
+            Assert.That(subject.ToString, Does.Contain("Calculator"));
+        }
+
+        [Test]
+        public void StringConstraints()
+        {
+            var subject = new Calculator {Num1 = 2, Num2 = 4};
+            var strResult = subject.ToString();
+            
+            Assert.That(strResult, Does.Contain("Calculator"));
+            Assert.That(strResult, Does.Not.Contain("Alex"));
+            Assert.That(strResult,Is.EqualTo("advancedNUnit.calculator").IgnoreCase);
+            Assert.That(strResult, Is.Not.Empty);
+        }
     }
     
 }
