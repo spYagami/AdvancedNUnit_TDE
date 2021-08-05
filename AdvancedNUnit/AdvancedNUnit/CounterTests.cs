c10af41e29f8e9b7dbabdec739540eb74c478893
Sergio Pessegueiro <iFrancisco.sp@icloud.com>
2021-08-05 14:15:02 +0100

    Add an example of a TestCaseSource.
    These allow me to input large amounts of information defined somewhere else.

--- AdvancedNUnit/AdvancedNUnit/CounterTests.cs
+++ AdvancedNUnit/AdvancedNUnit/CounterTests.cs
@@ -11,21 +11,38 @@ namespace AdvancedNUnit
         [SetUp]
         public void SetUp()
         {
-            _sut = new Counter(6);
+            _sut = new Counter(0);
         }
         
         [Test]
         public void Increment_IncreaseCountByOne()
         {
             _sut.Increment();
-            Assert.That(_sut.Count, Is.EqualTo(7));
+            Assert.That(_sut.Count, Is.EqualTo(1));
         }
         
         [Test]
         public void Decrement_DecreasesCountByOne()
         {
             _sut.Decrement();
-            Assert.That(_sut.Count, Is.EqualTo(5));
+            Assert.That(_sut.Count, Is.EqualTo(-1));
         }
+        
+        [Category("AddCases")]
+        [TestCaseSource("_addCases")]
+        public void Add_Always_ReturnsResult_WithDataProvider(int x, int y, int expResult)
+        {
+            var subject = new Calculator {Num1 = x, Num2 = y};
+            Assert.That(subject.Add(), Is.EqualTo(expResult));
+        }
+
+        private static object[] _addCases =
+        {
+            new int[] {2,4,6},
+            new int[] {-2,3,1},
+            new int[] {3,3,6},
+            new int[] {33,3,36}
+        };
+
     }
 }
